ebeaddb2bc9d1a8563d4bbba9964a67f3142e3bb
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-04-08 14:06:46 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -24,7 +24,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001734555079"))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "EXOTICMUSIC BOT")
 
 OWNER_ID = list(
     map(int, getenv("OWNER_ID", "5537497510").split())
