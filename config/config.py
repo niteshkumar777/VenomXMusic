5a8149ad5e909f96602f4691399ea828871befa9
Nitesh Kumar <126387824+niteshkumar777@users.noreply.github.com>
2023-05-15 14:14:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "url=f"https://te.legra.ph/file/4eed66c1f998a39b16de4.mp4",
+    "https://github.com/niteshkumar777/VenomXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
